92f9e7bc56484bf39a2e63ff0d4e926f878fc200
Emissions <unitedrealm@hotmail.com>
2023-06-06 16:18:00 +0100

    updated

--- lang/core/Welcome/events/join/JoinEventLang.d.ts
+++ lang/core/Welcome/events/join/JoinEventLang.d.ts
@@ -1,9 +1,12 @@
 import { BaseLang } from "../../../BaseLang.js";
 export declare class JoinEventLang extends BaseLang {
-    welcome(tag: string, username: string, discrim: string, memberNumber: string, memberNumberEnding: string): {
+    welcome(tag: string, username: string, discrim: string, memberNumber: string, memberNumberEnding: string, avatarUrl: string): {
         author: {
             name: string;
         };
         description: string | undefined;
+        thumbnail: {
+            url: string;
+        };
     };
 }
--- lang/src/BaseApp.d.ts
+++ lang/src/BaseApp.d.ts
@@ -1,6 +1,6 @@
 import Base from "ts-modular-bot-file-design";
 import { Dependency } from "ada-types";
-import { JoinEventLang } from "../core/Welcome/events/join/JoinEventLang";
+import { JoinEventLang } from "../core/Welcome/events/join/JoinEventLang.js";
 declare abstract class BaseApp extends Base {
     constructor();
     Welcome: {
